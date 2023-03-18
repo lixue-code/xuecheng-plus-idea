0c4cf707c0d1e6b58e794e09fb1a28de810badbc
lixue-code <1760736397@qq.com>
2023-03-18 17:22:27 +0800

    项目基本搭建完成

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XuechengPlusBaseApplication {
-
+public class ContentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(XuechengPlusBaseApplication.class, args);
+        SpringApplication.run(ContentApplication.class, args);
     }
-
 }
